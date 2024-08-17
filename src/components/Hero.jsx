bc1c1921f6de452ce1684a95e40b0a24c0ad243f
jakubharnak <harnakjakub@gmail.com>
2024-08-17 21:25:43 +0200

    education-component
    added new component with my schools

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const container = (delay) => ({
 
 const Hero = () => {
   return (
-    <div className="border-b border-transparent pb-5 lg:mb-35">
+    <div className="border-b border-transparent pb-6 lg:mb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
--- src/constants/index.js
+++ src/constants/index.js
@@ -13,32 +13,31 @@ I am always eager to learn and take on new challenges, aiming to leverage my ski
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
+    year: "2019 - Present",
+    role: "IT Support Technician",
+    company: "Harňák s.r.o.",
     description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
     technologies: ["Python", "Svelte", "Three.js", "Postgres"],
   },
   {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
+    year: "2021 - 2021",
+    role: "IT Support Technician",
+    company: "Leoni",
     description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
     technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const EDUCATION = [
+  {
+    year: "2023 - Present",
+    school: "Brno University of Technology",
+    type: "Fakulta informačních technologií",
+  },
+  {
+    year: "2019-2023",
+    school: "SOŠ Informačních technologií sv. Jána Bosca",
+    type: "Informatika a elektrotechnika",
   },
 ];
 
@@ -74,7 +73,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Na záhumní, Klobušice , Slovakia",
+  phoneNo: "+421948257018",
+  email: "harnakjakub@gmail.com",
 };
